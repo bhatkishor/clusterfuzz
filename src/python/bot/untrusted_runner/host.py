96b63147de71558e49fe58ff8c76f4db966c02d0
Oliver Chang <ochang@google.com>
2019-12-19 16:17:49 +1100

    Replace App Engine NDB with google cloud NDB.
    DO NOT MERGE YET.
    
    Part of migration to Python 3. This is our last major blocker and
    dependency on the Python 2 App Engine SDK.
    
    Also remove ndb_patcher, as that is no longer required.

--- src/python/bot/untrusted_runner/host.py
+++ src/python/bot/untrusted_runner/host.py
@@ -20,12 +20,12 @@ import sys
 import threading
 import time
 
+from google.cloud import ndb
 import grpc
 
 from base import untrusted
 from base import utils
 from datastore import data_types
-from datastore import ndb
 from metrics import logs
 from metrics import monitoring_metrics
 from protos import heartbeat_pb2
